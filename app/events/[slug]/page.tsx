2dadd8cb44abc71f70eba0ef1b00983a30debd3d
ronnyr <ronnyr@wix.com>
2022-12-28 13:28:24 +0200

    more settings and styling

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,9 +1,10 @@
 import { useServerAuthSession } from '@app/hooks/useServerAuthSession';
 import { getEventBySlug } from '@model/event/event-api';
-import { TicketsTable } from '@app/components/Table/Table.client';
 import { getTicketsById } from '@model/availability/availability-api';
 import { WixMediaImage } from '@app/components/Image/WixMediaImage';
 import { formatDate } from '@app/utils/date-formatter';
+import { RegistrationStatus } from '@model/event/types';
+import { TicketsTable } from '@app/components/Table/Table.client';
 
 export default async function EventPage({ params }: any) {
   const wixSession = useServerAuthSession();
@@ -12,15 +13,44 @@ export default async function EventPage({ params }: any) {
   return (
     <div className="max-w-full-content mx-auto px-14">
       {event ? (
-        <div className="full-w overflow-hidden max-w-7xl mx-auto">
+        <div className="full-w overflow-hidden max-w-7xl mx-auto text-center">
           <span>
             {formatDate(new Date(event.scheduling?.config?.startDate!)) ||
               event.scheduling?.formatted}{' '}
             | {event.location?.name}
           </span>
           <h1 className="text-4xl mb-2">{event.title}</h1>
-          <h3>{event.description}</h3>
-          <WixMediaImage media={event.mainImage} />
+          <h3 className="my-10">{event.description}</h3>
+          {event.registration?.status === RegistrationStatus.OPEN_TICKETS && (
+            <a className="btn-main inline-block" href="#tickets">
+              Buy Tickets
+            </a>
+          )}
+          {event.registration?.status === RegistrationStatus.OPEN_EXTERNAL && (
+            <a
+              className="btn-main inline-block"
+              href={event.registration.external!.registration}
+            >
+              Buy Tickets
+            </a>
+          )}
+          {[
+            RegistrationStatus.CLOSED_MANUALLY,
+            RegistrationStatus.CLOSED,
+          ].includes(event.registration?.status!) && (
+            <div>
+              <p className="border-2 inline-block p-3">
+                Registration is closed
+                <br />
+                <a href="/" className="underline">
+                  See other events
+                </a>
+              </p>
+            </div>
+          )}
+          <div className="my-10">
+            <WixMediaImage media={event.mainImage} />
+          </div>
           <h2 className="text-center mt-7">TIME & LOCATION</h2>
           <p className="text-center">{event.scheduling?.formatted}</p>
           {event.about !== '<p></p>' ? (
@@ -35,11 +65,24 @@ export default async function EventPage({ params }: any) {
           <p className="text-center">
             {event.location?.fullAddress?.formattedAddress}
           </p>
-          <TicketsTable
-            tickets={tickets!}
-            event={event}
-            wixSession={wixSession}
-          />
+          {event.registration?.external && (
+            <a
+              className="btn-main my-10 inline-block"
+              href={event.registration?.external.registration}
+            >
+              Buy Tickets
+            </a>
+          )}
+          {[
+            RegistrationStatus.CLOSED_MANUALLY,
+            RegistrationStatus.OPEN_TICKETS,
+          ].includes(event.registration?.status!) && (
+            <TicketsTable
+              tickets={tickets!}
+              event={event}
+              wixSession={wixSession}
+            />
+          )}
         </div>
       ) : (
         <div className="text-3xl w-full text-center p-9 box-border">
