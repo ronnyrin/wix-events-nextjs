f859c775d0803938ed71a819fd5a7dec6db43c4f
ronnyr <ronnyr@wix.com>
2023-02-12 16:50:05 +0200

    shop responsive

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -6,17 +6,17 @@ export default async function StoresCategoryPage() {
   const { items } = await wixClient.products.queryProducts().limit(10).find();
   return (
     <div className="mx-auto">
-      <div className="bg-black text-custom-1 text-center py-20 h-[560px]">
-        <h1 className="uppercase text-6xl">Merch</h1>
-        <p className="text-xl mx-auto max-w-[60%] my-10">
+      <div className="bg-black text-custom-1 text-center py-4 sm:py-10 sm:py-20 h-[450px] sm:h-[520px]">
+        <h1 className="uppercase text-3xl sm:text-6xl">Merch</h1>
+        <p className="text-sm sm:text-base mx-auto px-8 sm:max-w-[50%] my-10">
           I’m a paragraph. I’m a great space to write about what makes the
           products special and explain how customers can benefit from these
           items.
         </p>
       </div>
       {items.length ? (
-        <div className="full-w overflow-hidden mx-auto text-center mt-[-100px] px-10">
-          <ul className="grid grid-cols-3 gap-4 grid-flow-row">
+        <div className="full-w overflow-hidden mx-auto text-center mt-[-200px] sm:mt-[-130px] px-10">
+          <ul className="grid sm:grid-cols-3 gap-4 grid-flow-row">
             {items.map((item) => (
               <li key={item._id} className="">
                 <a href={`/product-page/${item.slug}`}>
