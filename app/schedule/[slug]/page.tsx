ccd808e8460b381d0fecaf73c667c3867db0a451
ronnyr <ronnyr@wix.com>
2023-02-12 16:16:36 +0200

    schedule responsive

--- app/schedule/[slug]/page.tsx
+++ app/schedule/[slug]/page.tsx
@@ -14,25 +14,31 @@ export default async function SchedulePage({ params }: any) {
   });
 
   return (
-    <div className="max-w-3xl mx-auto px-14 pt-16">
-      <p className="font-helvetica">
-        {event!.title} | {event!.scheduling?.formatted},{' '}
-        {event!.location!.address}
-      </p>
-      <h1 className="text-5xl mb-12 mt-4">Schedule</h1>
-      <a
-        className="text-purple-500 border py-2 px-4 mb-8 border-purple-500 inline-block"
-        href={`/events/${params.slug}`}
-      >
-        Buy Tickets
-      </a>
-      <h2 className="mt-4 border-b border-black pb-4">
-        {formatDate(
-          new Date(event!.scheduling?.config?.startDate!),
-          event!.scheduling!.config!.timeZoneId!
-        )}
-      </h2>
-      <Schedule items={items!} slug={params.slug} isFull={true} />
+    <div className="max-w-4xl mx-auto px-8 sm:px-14 pt-4 sm:pt-16">
+      <div className="flex flex-col-reverse sm:flex-col">
+        <p className="font-helvetica text-xs sm:text-base">
+          {event!.title} | {event!.scheduling?.formatted},{' '}
+          {event!.location!.address}
+        </p>
+        <h1 className="text-2xl sm:text-5xl mb-4 sm:mb-12 sm:mt-4">Schedule</h1>
+      </div>
+      <div className="flex flex-col-reverse sm:flex-col">
+        <a
+          className="text-purple-500 border py-2 mt-4 sm:mt-0 px-4 sm:mb-8 border-purple-500 inline-block w-full sm:w-fit text-center"
+          href={`/events/${params.slug}`}
+        >
+          Get Tickets
+        </a>
+        <div>
+          <h2 className="mt-4 border-b border-black pb-4">
+            {formatDate(
+              new Date(event!.scheduling?.config?.startDate!),
+              event!.scheduling!.config!.timeZoneId!
+            )}
+          </h2>
+          <Schedule items={items!} slug={params.slug} isFull={true} />
+        </div>
+      </div>
     </div>
   );
 }
