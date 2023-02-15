13a3f1f9681455b1e14e04eebe9f7cf6d1b3c4c8
ronnyr <ronnyr@wix.com>
2023-02-15 12:40:36 +0200

    protect apis

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,50 @@
 import Image from 'next/image';
 import { getWixClient } from '@app/hooks/useWixClientServer';
 import { wixEvents } from '@wix/events';
+import { products } from '@wix/stores';
 import { WixMediaImage } from '@app/components/Image/WixMediaImage';
 import { getDatePart } from '@app/utils/date-formatter';
 
 export default async function Home() {
   const wixClient = await getWixClient();
-  const { items: collectionsItems } = await wixClient.collections
-    .queryCollections()
-    .ne('id', '00000000-000000-000000-000000000001')
-    .limit(3)
-    .find();
-  const productsForCategories = await Promise.all(
-    collectionsItems.map((collection) =>
-      wixClient.products
-        .queryProducts()
-        .eq('collections.id', collection._id)
-        .limit(1)
-        .find()
-        .then((products) => ({
-          product: products.items[0],
-          category: collection.name,
-        }))
-    )
-  );
+  let productsForCategories: { category: string; product: products.Product }[] =
+    [];
+  try {
+    const { items: collectionsItems } = await wixClient.collections
+      .queryCollections()
+      .ne('id', '00000000-000000-000000-000000000001')
+      .limit(3)
+      .find();
+    productsForCategories = await Promise.all(
+      collectionsItems.map((collection) =>
+        wixClient.products
+          .queryProducts()
+          .eq('collections.id', collection._id)
+          .limit(1)
+          .find()
+          .then((products) => ({
+            product: products.items[0],
+            category: collection.name!,
+          }))
+      )
+    );
+  } catch (e) {}
 
-  const { events } = await wixClient.wixEvents.queryEventsV2({
-    fieldset: [wixEvents.EventFieldset.FULL, wixEvents.EventFieldset.DETAILS],
-    query: {
-      paging: { limit: 10, offset: 0 },
-      sort: [{ fieldName: 'start', order: wixEvents.SortOrder.ASC }],
-    },
-  });
+  let events: wixEvents.Event[] = [];
+  try {
+    events = (
+      await wixClient.wixEvents.queryEventsV2({
+        fieldset: [
+          wixEvents.EventFieldset.FULL,
+          wixEvents.EventFieldset.DETAILS,
+        ],
+        query: {
+          paging: { limit: 10, offset: 0 },
+          sort: [{ fieldName: 'start', order: wixEvents.SortOrder.ASC }],
+        },
+      })
+    ).events!;
+  } catch (e) {}
   return (
     <div className="mx-auto relative">
       <div className="relative">
@@ -117,145 +130,151 @@ export default async function Home() {
           height={245}
         />
       </div>
-      <div className="bg-zinc-900 text-site pt-24 sm:py-20 mt-[-75px]">
-        <div className="px-8 sm:px-14">
-          <h1 className="uppercase text-4xl sm:text-7xl text-center sm:text-left">
-            SHOWS
-          </h1>
-          <div className="py-10 sm:px-44">
-            {events!.map((event) => (
-              <div
-                className="flex border-b last:border-0 gap-4 sm:gap-8 flex-col sm:flex-row py-4 sm:py-0"
-                key={event._id}
-              >
-                <a
-                  href={`/events/${event.slug}`}
-                  className="flex flex-1 sm:items-center gap-4 sm:gap-8 flex-col sm:flex-row"
+      {events?.length ? (
+        <div className="bg-zinc-900 text-site pt-24 sm:py-20 mt-[-75px]">
+          <div className="px-8 sm:px-14">
+            <h1 className="uppercase text-4xl sm:text-7xl text-center sm:text-left">
+              SHOWS
+            </h1>
+            <div className="py-10 sm:px-44">
+              {events!.map((event) => (
+                <div
+                  className="flex border-b last:border-0 gap-4 sm:gap-8 flex-col sm:flex-row py-4 sm:py-0"
+                  key={event._id}
                 >
-                  <div className="flex flex-col sm:flex-row gap-8 relative">
-                    <div className="w-[310px] h-[171px] sm:w-[80px] sm:h-[80px] overflow-hidden">
-                      <WixMediaImage
-                        media={event.mainImage}
-                        width={300}
-                        height={300}
-                      />
-                    </div>
-                    <div className="flex gap-4 items-center absolute bottom-2 left-2 sm:bottom-auto sm:left-auto sm:relative">
-                      <span className="text-4xl">
-                        {getDatePart(
-                          new Date(event.scheduling?.config?.startDate!),
-                          'day',
-                          event!.scheduling!.config!.timeZoneId!
-                        )}
-                      </span>
-                      <div className="flex flex-col text-xs">
-                        <span className="text-white sm:text-gray-600">
+                  <a
+                    href={`/events/${event.slug}`}
+                    className="flex flex-1 sm:items-center gap-4 sm:gap-8 flex-col sm:flex-row"
+                  >
+                    <div className="flex flex-col sm:flex-row gap-8 relative">
+                      <div className="w-[310px] h-[171px] sm:w-[80px] sm:h-[80px] overflow-hidden">
+                        <WixMediaImage
+                          media={event.mainImage}
+                          width={300}
+                          height={300}
+                        />
+                      </div>
+                      <div className="flex gap-4 items-center absolute bottom-2 left-2 sm:bottom-auto sm:left-auto sm:relative">
+                        <span className="text-4xl">
                           {getDatePart(
                             new Date(event.scheduling?.config?.startDate!),
-                            'weekday',
+                            'day',
                             event!.scheduling!.config!.timeZoneId!
                           )}
                         </span>
-                        <span>
-                          {getDatePart(
-                            new Date(event.scheduling?.config?.startDate!),
-                            'month',
-                            event!.scheduling!.config!.timeZoneId!
-                          )}
-                        </span>
+                        <div className="flex flex-col text-xs">
+                          <span className="text-white sm:text-gray-600">
+                            {getDatePart(
+                              new Date(event.scheduling?.config?.startDate!),
+                              'weekday',
+                              event!.scheduling!.config!.timeZoneId!
+                            )}
+                          </span>
+                          <span>
+                            {getDatePart(
+                              new Date(event.scheduling?.config?.startDate!),
+                              'month',
+                              event!.scheduling!.config!.timeZoneId!
+                            )}
+                          </span>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <span className="text-2xl grow text-left">{event.title}</span>
-                </a>
-                <a
-                  className="btn-main my-2 sm:my-10 rounded-2xl w-full text-center sm:w-auto"
-                  href={`/events/${event.slug}`}
-                >
-                  Buy Tickets
-                </a>
-              </div>
-            ))}
+                    <span className="text-2xl grow text-left">
+                      {event.title}
+                    </span>
+                  </a>
+                  <a
+                    className="btn-main my-2 sm:my-10 rounded-2xl w-full text-center sm:w-auto"
+                    href={`/events/${event.slug}`}
+                  >
+                    Buy Tickets
+                  </a>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex gap-2 sm:gap-14 px-14 flex-col sm:flex-row">
-        <div className="text-custom-1 text-center sm:text-left pt-10 sm:py-20 basis-1/2 bg-site">
-          <h1 className="uppercase text-4xl sm:text-7xl text-center sm:text-left text-black">
-            Merch
-          </h1>
-          <p className="text-lg my-10 text-black">
-            I’m a paragraph. I’m a great space to write about what makes the
-            products special and explain how customers can benefit from these
-            items.
-          </p>
-          <a
-            href="/shop"
-            className="btn-main rounded-2xl text-base px-8 py-2.5"
-          >
-            Get Merch
-          </a>
-          <div className="mt-10 sm:mt-[300px]">
-            <a href="/shop">
-              <Image
-                src={
-                  productsForCategories[1]!.product!.media!.mainMedia!.image!
-                    .url!
-                }
-                width={800}
-                height={0}
-                alt={
-                  productsForCategories[1]!.product!.media!.mainMedia!.image!
-                    .altText!
-                }
-              />
+      ) : null}
+      {productsForCategories.length ? (
+        <div className="flex gap-2 sm:gap-14 px-14 flex-col sm:flex-row">
+          <div className="text-custom-1 text-center sm:text-left pt-10 sm:py-20 basis-1/2 bg-site">
+            <h1 className="uppercase text-4xl sm:text-7xl text-center sm:text-left text-black">
+              Merch
+            </h1>
+            <p className="text-lg my-10 text-black">
+              I’m a paragraph. I’m a great space to write about what makes the
+              products special and explain how customers can benefit from these
+              items.
+            </p>
+            <a
+              href="/shop"
+              className="btn-main rounded-2xl text-base px-8 py-2.5"
+            >
+              Get Merch
             </a>
-            <span className="font-bold text-2xl sm:text-5xl block text-center mt-[-15px] sm:mt-[-30px] text-black">
-              <a href="/shop">{productsForCategories[1].category}</a>
-            </span>
+            <div className="mt-10 sm:mt-[300px]">
+              <a href="/shop">
+                <Image
+                  src={
+                    productsForCategories[1]!.product!.media!.mainMedia!.image!
+                      .url!
+                  }
+                  width={800}
+                  height={0}
+                  alt={
+                    productsForCategories[1]!.product!.media!.mainMedia!.image!
+                      .altText!
+                  }
+                />
+              </a>
+              <span className="font-bold text-2xl sm:text-5xl block text-center mt-[-15px] sm:mt-[-30px] text-black">
+                <a href="/shop">{productsForCategories[1].category}</a>
+              </span>
+            </div>
+          </div>
+          <div>
+            <div className="mt-10 sm:mt-[220px]">
+              <a href="/shop">
+                <Image
+                  src={
+                    productsForCategories[0]!.product!.media!.mainMedia!.image!
+                      .url!
+                  }
+                  width={800}
+                  height={0}
+                  alt={
+                    productsForCategories[0]!.product!.media!.mainMedia!.image!
+                      .altText!
+                  }
+                />
+              </a>
+              <span className="font-bold text-2xl sm:text-5xl block text-center mt-[-15px] sm:mt-[-30px]">
+                <a href="/shop">{productsForCategories[0].category}</a>
+              </span>
+            </div>
+            <div className="mt-10 sm:mt-40">
+              <a href="/shop">
+                <Image
+                  src={
+                    productsForCategories[2]!.product!.media!.mainMedia!.image!
+                      .url!
+                  }
+                  width={800}
+                  height={0}
+                  alt={
+                    productsForCategories[2]!.product!.media!.mainMedia!.image!
+                      .altText!
+                  }
+                />
+              </a>
+              <span className="font-bold text-2xl sm:text-5xl block text-center mt-[-15px] sm:mt-[-30px]">
+                <a href="/shop">{productsForCategories[2].category}</a>
+              </span>
+            </div>
           </div>
         </div>
-        <div>
-          <div className="mt-10 sm:mt-[220px]">
-            <a href="/shop">
-              <Image
-                src={
-                  productsForCategories[0]!.product!.media!.mainMedia!.image!
-                    .url!
-                }
-                width={800}
-                height={0}
-                alt={
-                  productsForCategories[0]!.product!.media!.mainMedia!.image!
-                    .altText!
-                }
-              />
-            </a>
-            <span className="font-bold text-2xl sm:text-5xl block text-center mt-[-15px] sm:mt-[-30px]">
-              <a href="/shop">{productsForCategories[0].category}</a>
-            </span>
-          </div>
-          <div className="mt-10 sm:mt-40">
-            <a href="/shop">
-              <Image
-                src={
-                  productsForCategories[2]!.product!.media!.mainMedia!.image!
-                    .url!
-                }
-                width={800}
-                height={0}
-                alt={
-                  productsForCategories[2]!.product!.media!.mainMedia!.image!
-                    .altText!
-                }
-              />
-            </a>
-            <span className="font-bold text-2xl sm:text-5xl block text-center mt-[-15px] sm:mt-[-30px]">
-              <a href="/shop">{productsForCategories[2].category}</a>
-            </span>
-          </div>
-        </div>
-      </div>
+      ) : null}
     </div>
   );
 }
