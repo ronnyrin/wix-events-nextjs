75f6b87d06ec0577b442c8c0921a160fd4f7a183
ronnyr <ronnyr@wix.com>
2023-03-29 17:19:25 +0300

    oops

--- app/hooks/useWixClientServer.ts
+++ app/hooks/useWixClientServer.ts
@@ -1,11 +1,24 @@
 import { createClient, OAuthStrategy } from '@wix/api-client';
 import { collections, products } from '@wix/stores';
 import { orders } from '@wix/ecom';
-import { wixEvents, checkout, schedule } from '@wix/events';
+import {
+  wixEvents,
+  checkout,
+  schedule,
+  orders as eventOrders,
+} from '@wix/events';
 
 export const getWixClient = async () => {
   const wixClient = createClient({
-    modules: { products, collections, wixEvents, checkout, schedule, orders },
+    modules: {
+      products,
+      collections,
+      wixEvents,
+      checkout,
+      schedule,
+      orders,
+      eventOrders,
+    },
     auth: OAuthStrategy({ clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID! }),
   });
   const tokens = await wixClient.auth.generateVisitorTokens();
