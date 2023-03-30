405270349ef0be66cf1a61cade7d0bb293a3601f
ronnyr <ronnyr@wix.com>
2023-03-30 17:07:38 +0300

    member wip

--- app/components/Login/Login.tsx
+++ app/components/Login/Login.tsx
@@ -4,18 +4,20 @@ import Cookies from 'js-cookie';
 
 export const Login = () => {
   const wixClient = useWixClient();
+  const memberSession = Cookies.get('wixMemberSession');
+  const isLoggedIn = !!JSON.parse(memberSession || '{}').accessToken;
   const onLoginClick = async () => {
     const state = await wixClient.auth.generateOauthRedirectState(
       `${window.location.href}callback`
     );
     Cookies.set('oauthState', JSON.stringify(state), {
-      expires: 1,
+      expires: 0.1,
     });
     return wixClient.auth.signInWithRedirect(state);
   };
   return (
     <button onClick={onLoginClick} className="flex relative">
-      Login
+      {isLoggedIn ? 'Logout' : 'Login'}
     </button>
   );
 };
