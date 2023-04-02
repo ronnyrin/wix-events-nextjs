d0acf39692937a75b575d3554571191b62dea49c
ronnyr <ronnyr@wix.com>
2023-04-02 13:47:08 +0300

    member wip

--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -13,7 +13,9 @@ const Callback = () => {
     wixClient.auth
       .getMemberTokens(window.location.hash, oAuthState)
       .then((tokens) => {
-        Cookies.set('wixMemberSession', JSON.stringify(tokens.refreshToken));
+        Cookies.set('wixMemberSession', JSON.stringify(tokens.refreshToken), {
+          expires: 2,
+        });
         // Cookies.remove('oauthState');
         window.location.href = 'https://wix-events-nextjs.vercel.app/';
       });
