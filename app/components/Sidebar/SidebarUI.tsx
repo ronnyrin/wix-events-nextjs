f713494dd15a63d3856c0340968a3c848cf275a5
ronnyr <ronnyr@wix.com>
2023-04-17 15:41:03 +0300

    cart page

--- app/components/Sidebar/SidebarUI.tsx
+++ app/components/Sidebar/SidebarUI.tsx
@@ -1,14 +1,14 @@
 'use client';
 import React from 'react';
 import { Sidebar } from './Sidebar';
-import { CartSidebarView } from '../CartSidebar/CartSidebarView';
+import { CartView } from '../CartSidebar/CartView';
 import { useUI } from '../Provider/context';
 
 export const SidebarUI: React.FC = () => {
   const { displaySidebar, closeSidebar } = useUI();
   return displaySidebar ? (
     <Sidebar onClose={closeSidebar}>
-      <CartSidebarView />
+      <CartView />
     </Sidebar>
   ) : null;
 };
